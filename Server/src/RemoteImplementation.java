2aaac5ceb3f7817a26b803e41c15b7e9eb94264a
Pranav Saxena <psbits@gmail.com>
2014-03-13 20:41:15 -0400

    Log in functionality logic for writing to the LOG FILE

--- Server/src/RemoteImplementation.java
+++ Server/src/RemoteImplementation.java
@@ -7,9 +7,18 @@
  */
 
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -20,11 +29,42 @@ import java.util.logging.Logger;
  * @author zmolo_000
  */
 public class RemoteImplementation extends UnicastRemoteObject implements RemoteInterface {
-
-    public RemoteImplementation() throws RemoteException {
+   BufferedWriter writer = null;
+   static int count =0;
+   public RemoteImplementation() throws RemoteException {
 
     }
-
+    
+  
+    public Writer writeToLogFile(String login, String timeStamp, int flag) {
+        try {
+             
+               writer = new BufferedWriter(new FileWriter("logFile.txt", true));
+               if(count == 0){
+               writer.append("\n UserName   \t    Type      \t     TimeStamp ");
+               writer.newLine();
+               count++;
+               }              
+            } 
+            catch (IOException ex) {}       
+       try{
+        if(flag == 1){
+            writer.append("\n" +"  "+ login + "         logged in at         " + timeStamp);
+            writer.newLine();
+        }
+        else {
+            writer.append(login + "       logged out at         " + timeStamp );
+            writer.newLine();
+        }
+                    
+        writer.write("\n");
+        writer.close();
+       }
+       catch(IOException ex){}
+        return writer;
+    }
+    
+    
     @Override
     public LinkedList<InventoryItem> listInventory(String table) throws RemoteException {
         LinkedList<InventoryItem> result = new LinkedList<>();
@@ -81,8 +121,13 @@ public class RemoteImplementation extends UnicastRemoteObject implements RemoteI
     @Override
     public String login(String login, String password) throws RemoteException {
         String result = "badAuthorization";
+        
         try {
             result = DBHelper.login(login, password);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            System.out.println(result);
+            if(result != null )
+                writeToLogFile(login,timeStamp,1);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(RemoteImplementation.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
@@ -90,7 +135,16 @@ public class RemoteImplementation extends UnicastRemoteObject implements RemoteI
         }
         return result;
     }    
-
+    
+   /*
+    public String logout( String logout) throws RemoteException{
+        String res="logout";
+        
+        String logoutTimestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+        writeToLogFile(logout, logoutTimestamp,0);
+       return res;
+    }
+    */
     @Override
     public void submitOrder(String firstName, String lastName, String customerAddress,
             String phoneNumber, float fCost, List<InventoryItem> items) throws RemoteException {
